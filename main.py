a2ae4028b8a432ef88be3d3d6a1bd7917de03aaa
zhangliang-2021 <liangzhang2021@outlook.com>
2026-01-16 11:52:36 +0800

    Refactor Jenkins job handling with retry logic

--- main.py
+++ main.py
@@ -6,13 +6,64 @@ import json
 import logging
 import os
 from api4jenkins import Jenkins
+from collections import deque
 from time import time, sleep
 
 
+MAX_ATTEMPTS = 3
 log_level = os.environ.get("INPUT_LOG_LEVEL", "INFO")
 logging.basicConfig(format="JENKINS_ACTION: %(message)s", level=log_level)
 
-logging.getLogger('httpx').setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+
+def build_job(jenkins, job_name, interval, start_timeout, timeout, parameters):
+    queue_item = jenkins.build_job(job_name, **parameters)
+    logging.info("Start to start jenkins job")
+
+    t0 = time()
+    sleep(interval)
+    while time() - t0 < start_timeout:
+        build = queue_item.get_build()
+        if build:
+            break
+        logging.info(f"Waiting for starting jenkins job. Waiting {interval} seconds.")
+        sleep(interval)
+    else:
+        raise Exception(
+            f"Timeout to start jenkins job. Waited for {start_timeout} seconds."
+        )
+
+    job_console = f"{build.url}console"
+    logging.info(f"jenkins job url:{job_console}")
+
+    t1 = time()
+    sleep(interval)
+    while time() - t1 < timeout:
+        result = build.result
+        if result:
+            return (result, job_console, build.console_text())
+        sleep(interval)
+    else:
+        return ("TIMEOUT", job_console, None)
+
+
+def get_last_n_lines(generator, n=50):
+    last_n = deque(maxlen=n)
+    for item in generator:
+        last_n.append(item)
+    return list(last_n)
+
+
+def check_console_text(console_text):
+    error_string1 = "Caused: hudson.remoting.ProxyException: hudson.remoting.RequestAbortedException: java.nio.channels.ClosedChannelException"
+    error_string2 = (
+        "hudson.remoting.ProxyException: java.nio.channels.ClosedChannelException"
+    )
+    return any([error_string1 in x for x in console_text]) and any(
+        [error_string2 in x for x in console_text]
+    )
+
 
 def main():
     # jenkins info
@@ -50,42 +101,30 @@ def main():
 
     parameters = {"pr_number": pr_num, "branch": project_branch}
 
-    queue_item = jenkins.build_job(job_name, **parameters)
-    logging.info("Start to start jenkins job")
-
-    t0 = time()
-    sleep(interval)
-    while time() - t0 < start_timeout:
-        build = queue_item.get_build()
-        if build:
-            break
-        logging.info(f"Waiting for starting jenkins job. Waiting {interval} seconds.")
-        sleep(interval)
-    else:
-        raise Exception(
-            f"Timeout to start jenkins job. Waited for {start_timeout} seconds."
+    for attempt in range(1, MAX_ATTEMPTS + 1):
+        result, job_console_url, console_text = build_job(
+            jenkins, job_name, interval, start_timeout, timeout, **parameters
         )
-
-    job_console = f"{build.url}console"
-    logging.info(f"jenkins job url:{job_console}")
-    
-    t1 = time()
-    sleep(interval)
-    while time() - t1 < timeout:
-        result = build.result
         if result == "SUCCESS":
             logging.info("Build successful 🎉")
             with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
-                print(f"log_url={job_console}", file=fh)
-            print(f"::notice title=log_url::{job_console}")
+                print(f"log_url={job_console_url}", file=fh)
+            print(f"::notice title=log_url::{job_console_url}")
             return
-        elif result in ("FAILURE", "ABORTED", "UNSTABLE"):
+        elif result == "TIMEOUT":
+            raise Exception(
+                f"Build has not finished and timed out. Waited for {timeout} seconds."
+            )
+        elif result == "FAILURE":
+            last_console_text = get_last_n_lines(console_text)
+            if check_console_text(last_console_text):
+                continue
+            else:
+                raise Exception(
+                    f'Build status returned "{result}". Build has failed ☹️.'
+                )
+        else:
             raise Exception(f'Build status returned "{result}". Build has failed ☹️.')
-        sleep(interval)
-    else:
-        raise Exception(
-            f"Build has not finished and timed out. Waited for {timeout} seconds."
-        )
 
 
 if __name__ == "__main__":
